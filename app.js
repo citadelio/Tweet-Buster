e32b59dfc111a657b633dd852f79b725acc1c6d9
Citadel_DevsPie <warrakehussein@gmail.com>
2018-03-27 14:06:26 +0100

    init

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const Twitter = require('twitter');
+const port = 3986;
+const Tauth = require('./config/secrete'); 
+const fetch = require('node-fetch');
+const app = express();
+
+let tweetArray = [];
+let client = new Twitter({
+  consumer_key: Tauth.consumer_key,
+  consumer_secret: Tauth.consumer_secret,
+  access_token_key: Tauth.access_token_key,
+  access_token_secret: Tauth.access_token_secret
+});
+ 
+let params = {screen_name: 'citadel50'};
+
+setInterval( () => {
+let randomYear = new Date().getFullYear() - Math.floor(Math.random()*100);
+
+fetch('http://numbersapi.com/'+randomYear+'/year')
+	.then(res => res.text())
+	.then( data => {
+		client.post('statuses/update', {status: data}, function(error, tweet, response) {
+  if (!error) {
+    console.log(error);
+  }
+  console.log('A new tweet has been sent');
+  tweetArray.push(randomYear)
+  console.log(tweetArray.length+'  posts made');
+});
+	})
+	.catch( err => console.log(err))
+
+
+}, 21600000)
+
+app.listen(process.env.PORT || port, () => {
+	console.log(`Server started on port ${port}`)
+})
