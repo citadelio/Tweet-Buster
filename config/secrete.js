dfd0c337307e949c504638c8e70ce8f701c0b33f
Citadel_DevsPie <warrakehussein@gmail.com>
2018-03-27 15:43:21 +0100

    removed ignore

---
+++ config/secrete.js
@@ -0,0 +1,6 @@
+module.exports = {
+	 consumer_key: 'NIdfy0fk5YGcRti8mFdvSOVWB',
+  consumer_secret: 'WeDHJYq5uK8NeFXL6KVTa8lXwMa9WjBsMzadhHoLicmV2DgFrX',
+  access_token_key: '586786732-691r62co0tayXCph4sovw5Yh1WDN8bItx9WO6H0g',
+  access_token_secret: 'NOi7u5nKSKr2JNvqPt3c8QVwPMXVbc5vaFJh40yMgI4mn'
+}
